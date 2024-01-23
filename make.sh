7726a8dfe1e416ed281717aec875f815c238fc1c
omayosilver <133324534+omayosilver@users.noreply.github.com>
2024-01-23 22:24:25 +0300

    Update make.sh

--- make.sh
+++ make.sh
@@ -10,16 +10,4 @@ elif [ $FUNCTIONAL_MODE -eq 1 ]
   export OMP_NUM_THREADS=$proc
   ./rowStripe_openmp $rows $cols $size
   mpirun -np $proc ./mvm_rs_mpi $rows $cols $size
-elif [ $FUNCTIONAL_MODE -eq 2 ]
-  then
-  echo "Performing Column-Wise Matrix-Vector Multiplication with OpenMP and MPI\n"
-  export OMP_NUM_THREADS=$proc
-  ./columnStripe_openmp $rows $cols $size
-  mpirun -np $proc ./mvm_cs_mpi $rows $cols $size
-elif [ $FUNCTIONAL_MODE -eq 3 ]
-  then
-  echo "Performing Chckerboard Matrix-Vector Multiplication with OpenMP and MPI\n"
-  export OMP_NUM_THREADS=$proc
-  ./checkerboard_openmp $rows $cols $size $OMP_NUM_THREADS
-  mpirun -np $proc ./mvm_checkerboard $rows $cols $size
 fi
